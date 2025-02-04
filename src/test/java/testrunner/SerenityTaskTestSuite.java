9aa92cee01b9fd1452e8262de30f3c46b6d12ff4
vanshraj singh <ervanshsingh@gmail.com>
2025-02-05 01:59:11 +0530

    Headless mode True, Glue constants added in runner.

--- src/test/java/testrunner/SerenityTaskTestSuite.java
+++ src/test/java/testrunner/SerenityTaskTestSuite.java
@@ -1,15 +1,16 @@
 package testrunner;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.platform.engine.Constants;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
-@CucumberOptions(glue = "stepDefinitions")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions")
 public class SerenityTaskTestSuite {
 
 }
